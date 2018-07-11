3bf883d216966c634e3069cad2b91e7a55cd5f8d
Justin Stevens <justin@interactiveshift.com>
2018-07-11 10:39:09 -0700

    init

---
+++ ImageRecognizer.ts
@@ -0,0 +1,16 @@
+import { TfImageRecognition } from 'react-native-tensorflow';
+
+export default class ImageRecognizer
+{
+  recognizer: TfImageRecognition;
+  constructor(options)
+  {
+    this.recognizer = new TfImageRecognition(options);
+  }
+
+  async recognize(data)
+  {
+    return await this.recognizer.recognize(data);
+  }
+}
+
